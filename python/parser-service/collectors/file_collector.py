75f7ec4b3feb6b42993a94f7e43cbe6f4a68bda6
asharxh <asharxheikh47@gmail.com>
2026-05-25 01:54:07 +0530

    Real time log streaming

--- python/parser-service/collectors/file_collector.py
+++ python/parser-service/collectors/file_collector.py
@@ -1,4 +1,42 @@
-def collect_logs(file_path):
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 
-    with open(file_path, "r") as file:
-        return file.readlines()
+
+class LogFileHandler(FileSystemEventHandler):
+
+    def __init__(self, callback):
+        self.callback = callback
+
+    def on_modified(self, event):
+
+        if event.is_directory:
+            return
+
+        self.callback(event.src_path)
+
+
+def watch_log_file(path, callback):
+
+    event_handler = LogFileHandler(callback)
+
+    observer = Observer()
+
+    observer.schedule(
+        event_handler,
+        path=path,
+        recursive=False
+    )
+
+    observer.start()
+
+    print(f"[WATCHING] {path}")
+
+    try:
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
--- python/parser-service/main.py
+++ python/parser-service/main.py
@@ -1,4 +1,4 @@
-from collectors.file_collector import collect_logs
+from collectors.file_collector import watch_log_file
 from parsers.auth_parser import parse_auth_log
 from parsers.generic_parser import parse_generic_log
 from forwarders.api_forwarder import forward_log
@@ -8,19 +8,35 @@ from utils.log_reader import clean_log_line
 LOG_FILE = "../../sample-logs/auth.log"
 
 
+processed_lines = set()
+
+def process_log_file(file_path):
+
+    with open(file_path, "r") as file:
+
+        lines = file.readlines()
+
+        for line in lines:
+
+            clean_line = clean_log_line(line)
+
+            if clean_line in processed_lines:
+                continue
+            processed_lines.add(clean_line)
+            parsed_log = parse_auth_log(clean_line)
+
+            if not parsed_log:
+                parsed_log = parse_generic_log(clean_line)
+
+            forward_log(parsed_log)
+
+
 def main():
-
-    logs = collect_logs(LOG_FILE)
-
-    for line in logs:
-        clean_line = clean_log_line(line)
-        parsed_log = parse_auth_log(clean_line)
-
-        if not parsed_log:
-            parsed_log = parse_generic_log(clean_line)
-
-        forward_log(parsed_log)
-
+    process_log_file(LOG_FILE)
+    watch_log_file(
+        "../../sample-logs",
+        process_log_file
+    )
 
 if __name__ == "__main__":
     main()
